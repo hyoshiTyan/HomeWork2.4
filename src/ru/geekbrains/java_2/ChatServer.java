2a2edbc720357dde157c988d2004eeb66c14de3b
hyoshiTyan <sakuramisa@mail.ru>
2020-10-13 13:57:38 +0300

    Lesson 4

---
+++ src/ru/geekbrains/java_2/ChatServer.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.java_2;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
